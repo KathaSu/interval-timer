2ad872d54a3ac2aa5b72ba765506ce2a392d90d2
Amy Hoang <amy_hoang@outlook.de>
2021-04-04 10:02:53 +0200

    Login refactoring

--- src/app/timer-app/components/login/login.component.ts
+++ src/app/timer-app/components/login/login.component.ts
@@ -14,7 +14,6 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent extends BaseApiService implements OnInit, OnDestroy {  
-  stub = 'login/';
   stubType = ApiUrl;
 
   view = ViewType.Login;
@@ -22,46 +21,25 @@ export class LoginComponent extends BaseApiService implements OnInit, OnDestroy 
   showPw = false;
   submitted: boolean;
 
-  loginForm: FormGroup = new FormGroup({
-    email: new FormControl('', [  	
-      Validators.required,
-      Validators.pattern(LoginComponent.EMAIL_VALIDATOR),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      // TODO: Correct regex for password
-      // Minimum eight characters, at least one letter and one number:
-      // Validators.pattern("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"),
-    ]),
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', LoginComponent.EMAIL_VALIDATOR),
+    password: new FormControl(''),
+    password_confirm: new FormControl(''),
+    terms_conditions: new FormControl(''),
   });
 
-  forgotPwForm: FormGroup = new FormGroup({
-    email: new FormControl('', [  	
-      Validators.required,
-      Validators.pattern(LoginComponent.EMAIL_VALIDATOR),
-    ]),
-  });
+  private static readonly EMAIL_VALIDATOR = [  	
+    Validators.required,
+    Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+  ]; 
 
-  signUpForm: FormGroup = new FormGroup({
-    email: new FormControl('', [  	
-      Validators.required,
-      Validators.pattern(LoginComponent.EMAIL_VALIDATOR),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      // TODO: Correct regex for password
-      // Minimum eight characters, at least one letter, one number and one special character
-      // Validators.pattern("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"),
-    ]),
-    password_confirm: new FormControl('', [
-      Validators.required,
-    ]),
-    terms_conditions: new FormControl('', [
-      Validators.required, 
-    ])
-  });
-
-  private static readonly EMAIL_VALIDATOR = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'; 
+  private static readonly PASSWORD_VALIDATOR = [
+    Validators.required,
+    // TODO: Correct regex for password
+    // Minimum eight characters, at least one letter, one number and one special character
+    // Validators.pattern("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"),
+  ]
+  
   private destroy$ = new Subject();
 
   constructor(
@@ -72,17 +50,7 @@ export class LoginComponent extends BaseApiService implements OnInit, OnDestroy 
   }
 
   ngOnInit(): void {
-    // Observe form changes and then sets submitted to false
-    this.loginForm.valueChanges.subscribe(() => {
-      this.submitted = false;
-    });
-    this.forgotPwForm.valueChanges.subscribe(() => {
-      this.submitted = false;
-    });
-    this.signUpForm.valueChanges.subscribe(() => {
-      this.submitted = false;
-      this.setPwErrors(null);
-    });
+    this.formWatcher();
   }
   
   ngOnDestroy(): void {
@@ -91,25 +59,49 @@ export class LoginComponent extends BaseApiService implements OnInit, OnDestroy 
   }
 
   /**
-   * Login via email
+   * Change form and add/remove validators 
+   * @param type for setting validator to specific field 
    */
-  emailLogin(): void {
-    console.log('MUH', this.loginForm);
-    this.submitted = true;
-    if (this.loginForm.valid) {
-      // TODO: Adjust request response and error handling
-      this.post(this.loginForm.getRawValue())
-        .pipe(takeUntil(this.destroy$))
-        .subscribe(
-          () => { 
-            this.router.navigateByUrl('/timer-list');
-            this.loginForm.reset(); 
-          },
-          () => this.router.navigateByUrl('/timer-list'),
-        )
+  changeForm(type: number): void {
+    this.view = type;
+    this.form.reset();
+    this.form.clearValidators();
+
+    if (type === ViewType.Login || type === ViewType.SignUp) {
+      this.form.get('password').setValidators(LoginComponent.PASSWORD_VALIDATOR);
+    }
+
+    if (type === ViewType.SignUp) {
+      this.form.get('password_confirm').setValidators([Validators.required]);
+      this.form.get('terms_conditions').setValidators([Validators.required]);
+    }
+    
+    this.form.updateValueAndValidity();
+  }
+
+  /**
+   * Checks which method should be called for each form
+   */
+  submitForm(): void {
+    switch (this.view) {
+      case ViewType.SignUp:
+        this.signUp();
+        break;
+      case ViewType.ForgotPw: 
+        this.forgotPw();
+        break;
+      default:
+        this.emailLogin();
     }
   }
 
+  /**
+   * Reset submit after view change
+   */
+  resetSubmitted(): void {
+    this.submitted = false;
+  }
+    
   /**
    * Login via Google
    */
@@ -118,17 +110,39 @@ export class LoginComponent extends BaseApiService implements OnInit, OnDestroy 
   }
 
   /**
-   * Password forgotten - get password reset link
+   * Login via email
    */
-  forgotPw(): void {
+  private emailLogin(): void {
+    console.log('MUH', this.form);
     this.submitted = true;
-    if (this.forgotPwForm.valid) {
-      console.log('forgotPw', this.forgotPwForm.valid, this.forgotPwForm.getRawValue());
-      // TODO: Adjust request
-      this.post(this.forgotPwForm.getRawValue())
+    if (this.form.valid) {
+      // TODO: Adjust request response and error handling
+      this.stub = this.stubType.Login;
+      this.post(this.form.getRawValue())
         .pipe(takeUntil(this.destroy$))
         .subscribe(
-          () => this.forgotPwForm.reset(),
+          () => { 
+            this.router.navigateByUrl('/timer-list');
+            this.form.reset(); 
+          },
+          () => this.router.navigateByUrl('/timer-list'),
+        )
+    }
+  }
+
+  /**
+   * Password forgotten - get password reset link
+   */
+  private forgotPw(): void {
+    this.submitted = true;
+    if (this.form.valid) {
+      console.log('forgotPw', this.form.valid, this.form.getRawValue());
+      // TODO: Adjust request
+      this.stub = this.stubType.Login;
+      this.post(this.form.getRawValue())
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(
+          () => this.form.reset(),
           () => null,
         )
     }
@@ -138,43 +152,52 @@ export class LoginComponent extends BaseApiService implements OnInit, OnDestroy 
    * Sign up new user 
    * @returns If form is invalid or password don't match validator or both passwords don't match
    */
-     signUp(): void {
-      this.submitted = true;
-  
-      // Return if form is invalid
-      if (this.signUpForm.invalid) {
-        return;
-      }
-  
-      // Set error if passwords don't match
-      const rawForm: SignUpData = this.signUpForm.getRawValue();
-      if (rawForm.password !== rawForm.password_confirm) {
-        this.setPwErrors(true);
-        return;
-      }
-  
-      // TODO: Adjust request 
-      this.put(rawForm)
-        .pipe(takeUntil(this.destroy$))
-        .subscribe(
-          () => this.signUpForm.reset(),
-          () => null,
-        )
+  private signUp(): void {
+    this.submitted = true;
+
+    // Return if form is invalid
+    if (this.form.invalid) {
+      return;
     }
 
-    /**
-     * Reset submit after view change
-     */
-    resetSubmitted(): void {
+    // Set error if passwords don't match
+    const rawForm: SignUpData = this.form.getRawValue();
+    if (rawForm.password !== rawForm.password_confirm) {
+      this.setPwErrors(true);
+      return;
+    }
+
+    // TODO: Adjust request 
+    this.stub = this.stubType.SignUp;
+    this.put(rawForm)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        () => this.form.reset(),
+        () => null,
+      )
+  }
+
+  /**
+   * Observe form changes
+   */
+  private formWatcher(): void {
+    this.form.valueChanges.subscribe(() => {
+      // Sets submitted to false to hide input error styling until user submits form again
       this.submitted = false;
-    }
-  
-    /**
-     * Set form passwords errors
-     * @param value: true to set errors and null to reset errors 
-     */
-    private setPwErrors(value: true | null): void {
-      this.signUpForm.get('password').setErrors({'incorrect': value});
-      this.signUpForm.get('password_confirm').setErrors({'incorrect': value});
-    }
+
+      // Reset errors for password confirm in sign up form
+      if (this.view === this.viewType.SignUp) {
+        this.setPwErrors(null);
+      }
+    });
+  }
+
+  /**
+   * Set form passwords errors
+   * @param value: true to set errors and null to reset errors 
+   */
+  private setPwErrors(value: true | null): void {
+    this.form.get('password').setErrors({'incorrect': value});
+    this.form.get('password_confirm').setErrors({'incorrect': value});
+  }
 }
